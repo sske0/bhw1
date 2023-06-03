0a80052c0985683546e017e860f7d4b7adbf5e1a
sske0 <akbaraz@code.edu.az>
2023-06-03 19:26:30 +0400

    first commit

---
+++ PurpleBuzz/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PurpleBuzz.Models;
+using PurpleBuzz.ViewModels.About;
+
+namespace PurpleBuzz.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ours = new List<Our>
+             {
+                 new Our {Id = 1, Title="Our Vision" , Description="Vision desc", PhotoPath="bxs-bulb"},
+                 new Our {Id = 2, Title="Our Mission" , Description="Mission desc", PhotoPath="bx-revision"},
+                 new Our {Id = 3, Title="Our Goal" , Description="Goal desc", PhotoPath="bxs-select-multiple"}
+             };
+
+            ViewBag.WorksTitle = "bx-revision";
+
+            var model = new AboutIndexVM
+            {
+                Ours = ours
+            };
+            return View(model);
+        }
+    }
+}
---
+++ PurpleBuzz/ViewModels/About/AboutIndexVM.cs
@@ -0,0 +1,9 @@
+﻿using PurpleBuzz.Models;
+
+namespace PurpleBuzz.ViewModels.About
+{
+    public class AboutIndexVM
+    {
+        public List<Our> Ours { get; set; }
+    }
+}
